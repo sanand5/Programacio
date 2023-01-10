13e695e354e24129cb08309f444aea993b44f2a0
sanand5 <andsansan5@alu.edu.gva.es>
2023-01-10 18:13:15 +0100

    act20

---
+++ 2naAVA/UF07/UF07_20/src/uf07_20/UF07_20.java
@@ -0,0 +1,25 @@
+package uf07_20;
+
+// Andreu Sanz Sanz
+
+import java.util.Scanner;
+
+
+public class UF07_20 {
+    public static void fun(int num) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%2d x %d = %d\n",i,num,num*i);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Numero: ");
+        fun(sc.nextInt());
+        
+        
+        
+        
+        
+    }
+
+}
