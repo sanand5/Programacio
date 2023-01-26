c4c6e45bf919df3f59cdb108987627342765309b
sanand <andsansan5@alu.edu.gva.es>
2023-01-26 12:20:17 +0100

    c4

--- 2naAVA/UF08/UF08_C4/src/uf08_c4/Article_c.java
+++ 2naAVA/UF08/UF08_C4/src/uf08_c4/Article_c.java
@@ -9,9 +9,9 @@ package uf08_c4;
  * @author sanzs
  */
 public class Article_c {
-    String nom;
-    double preu, iva;
-    int quantsQueden;
+    private String nom;
+    private double preu, iva;
+    private int quantsQueden;
     public Article_c(String nom, double preu, double iva, int quantsQueden) {
         if (preu<=0 || iva<=0 || quantsQueden<0) System.out.println("***Aquests valors no son valids, no s'ha guardat cao valor***");
         else{
@@ -21,7 +21,35 @@ public class Article_c {
             this.quantsQueden=quantsQueden;
         }
     }
-    public void get(int num) {
-        
+    public String getnom() {
+        return this.nom;
     }
+    public double getpreu() {
+        return this.preu;
+    }
+    public double getiva() {
+        return this.iva;
+    }
+    public double getquantsQueden() {
+        return this.quantsQueden;
+    }
+    public void setnom(String nom ) {
+       this.nom=nom;
+    }
+    public void setpreu(double preu) {
+        if (this.preu<=0) {
+            System.out.println("El valor ha de ser major que 0");
+        }else this.preu=preu;
+    }
+    public void setiva(double iva) {
+        if (this.iva<0) {
+            System.out.println("El valor no pot ser menor a 0");
+        }else this.iva=iva;
+    }
+    public void setquantsQueden(int quantsQueden) {
+        if (this.quantsQueden<0) {
+            System.out.println("Rl vslor no pot ser menor que 0");
+        }else this.quantsQueden=quantsQueden;
+    }
+    
 }
--- 2naAVA/UF08/UF08_C4/src/uf08_c4/UF08_C4.java
+++ 2naAVA/UF08/UF08_C4/src/uf08_c4/UF08_C4.java
@@ -10,10 +10,10 @@ package uf08_c4;
 public class UF08_C4 {
     public static void main(String[] args) {
         Article_c A1 = new Article_c("Pijama", 10, 21, 0);
-        System.out.printf("%s - Preu:  %.0f€ - IVA: %.0f%% - PVP: %.2f€",A1.nom,A1.preu,A1.iva,(A1.preu+(A1.preu*A1.iva)/100));
-        A1.preu=10;
+        System.out.printf("%s - Preu:  %.0f€ - IVA: %.0f%% - PVP: %.2f€",A1.getnom(),A1.getpreu(),A1.getiva(),(A1.getpreu()+(A1.getpreu()*A1.getiva())/100));
+        A1.setpreu(-1);
         System.out.println();
-        System.out.printf("%s - Preu:  %.0f€ - IVA: %.0f%% - PVP: %.2f€",A1.nom,A1.preu,A1.iva,(A1.preu+(A1.preu*A1.iva)/100));
+        System.out.printf("%s - Preu:  %.0f€ - IVA: %.0f%% - PVP: %.2f€",A1.getnom(),A1.getpreu(),A1.getiva(),(A1.getpreu()+(A1.getpreu()*A1.getiva())/100));
     }
 
 }
