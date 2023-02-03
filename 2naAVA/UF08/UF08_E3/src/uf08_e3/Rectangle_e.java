3d2b179a5d34f1e1cdbff4564e3c729f13e59121
sanand <andsansan5@alu.edu.gva.es>
2023-02-03 20:00:22 +0100

    jaja

--- 2naAVA/UF08/UF08_E3/src/uf08_e3/Rectangle_e.java
+++ 2naAVA/UF08/UF08_E3/src/uf08_e3/Rectangle_e.java
@@ -10,15 +10,26 @@ package uf08_e3;
  */
 public class Rectangle_e {
     private int x1,x2,y1,y2;
+    final static int MIN=0, MAX=100;
     
     public Rectangle_e (int x1, int x2, int y1, int y2){
-        if (x1>=x2 || y1>=y2) System.out.println(".zsd,h we5n8òej5py695ewry oytvehir");
+        if (x1>=x2 || y1>=y2 || x1>MAX || x1<MIN|| x2>MAX || x2<MIN|| y1>MAX || y1<MIN|| y2>MAX || y2<MIN) System.out.println("ERROR");
         else {
             this.x1=x1;
             this.x2=x2;
             this.y1=y1;
             this.y2=y2;
         }
+    }
+    public static Rectangle_e rectangleAleatori() {
+        int a=(int) (Math.random() * (MAX - MIN + 1) + MIN);
+        int b=(int) (Math.random() * (MAX - MIN + 1) + MIN);
+        int c=(int) (Math.random() * (MAX - MIN + 1) + MIN);
+        int d=(int) (Math.random() * (MAX - MIN + 1) + MIN);
+        return new Rectangle_e(Math.min(a, b),Math.max(a, b),Math.min(c, d),Math.max(c, d));
+    }
+    public void imprimeix() {
+        System.out.printf("(%d,%d)/(%d,%d)%n",getX1(),getY1(),getX2(),getY2());
     }
     double getperimetre() {
         return (double)(Math.abs(getX1()-getX2())+Math.abs(getY1()-getY2()))*2;
@@ -37,23 +48,23 @@ public class Rectangle_e {
         
     }
     public void setX1(int x1) {
-        if (x1>=getX2()) {
+        if (x1>=getX2() || x1>MAX || x1<MIN ) {
             System.out.println("***Error, el valor ha de ser >0, el valor per defecte sera el anterior***");
         }else this.setX1(x1);
     }
     public void setX2(int x2) {
-        if (x2<=getX1()) {
+        if (x2<=getX1()|| x2>MAX || x2<MIN) {
             System.out.println("***Error, el valor ha de ser >0, el valor per defecte sera el anterior***");
         }else this.setX2(x2);
         
     }
     public void setY1(int y1) {
-        if (y1>=getY2()) {
+        if (y1>=getY2() || y1>MAX || y1<MIN) {
             System.out.println("***Error, el valor ha de ser >0, el valor per defecte sera el anterior***");
         }else this.setY1(y1);
     }
     public void setY2(int y2) {
-        if (y2<=getY1()) {
+        if (y2<=getY1() || y2>MAX || y2<MIN) {
             System.out.println("***Error, el valor ha de ser >0, el valor per defecte sera el anterior***");
         }else this.setY2(y2);
     }
--- 2naAVA/UF08/UF08_E3/src/uf08_e3/UF08_E3.java
+++ 2naAVA/UF08/UF08_E3/src/uf08_e3/UF08_E3.java
@@ -10,5 +10,11 @@ public class UF08_E3 {
         Rectangle_e r1=new Rectangle_e(2,4,6,7);
         r1.print();
         r1.setX1(11); //crida al setter pa que traga misatge
+        Rectangle_e V[] = new Rectangle_e[30];
+        for (int i = 0; i < 30; i++) {
+            V[i]=Rectangle_e.rectangleAleatori();
+            V[i].imprimeix();
+            System.out.println();
+        }
     }
 }
