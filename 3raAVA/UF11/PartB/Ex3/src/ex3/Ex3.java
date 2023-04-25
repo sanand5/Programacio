1d9cd06681812eab5a6b84067f1bd37ab8ec1821
sanand <andsansan5@alu.edu.gva.es>
2023-04-25 21:18:48 +0200

    d

---
+++ 3raAVA/UF11/PartB/Ex3/src/ex3/Ex3.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex3;
+
+import java.io.File;
+import java.util.Date;
+
+/**
+ *
+ * @author sanand
+ */
+public class Ex3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        File fitxer = new File("build.xml");
+        File direct = new File("build");
+        Date d = new Date(fitxer.lastModified());
+        System.out.println(d);
+    }
+    
+}
