63d5ab7eb0deda772cb02d9ad5c1d1a5586e82a5
sanand <andsansan5@alu.edu.gva.es>
2023-04-25 10:08:43 +0200

    casa

--- 3raAVA/UF11/Exemples/UF11_02/src/uf11_02/UF11_02.java
+++ 3raAVA/UF11/Exemples/UF11_02/src/uf11_02/UF11_02.java
@@ -24,37 +24,43 @@ public class UF11_02 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        Map<String, String> m = new HashMap<>();
-        m.put("manzana", "poma");
-        m.put("pera", "pera");
-        m.put("naranja", "taronja");
-        m.put("limón", "llimona");
-        m.put("plátano", "plàtan");
-        m.put("uva", "raïm");
-        m.put("fresa", "maduixa");
-        m.put("arándano", "blauet");
-        m.put("frambuesa", "framboesa");
-        m.put("piña", "pinya");
-        m.put("melón", "meló");
-        m.put("sandía", "síndria");
-        m.put("zanahoria", "pastanaga");
-        m.put("cebolla", "ceba");
-        m.put("ajo", "all");
-        m.put("pimiento", "pebrot");
-        m.put("tomate", "tomàquet");
-        m.put("patata", "patata");
-        m.put("maíz", "blat de moro");
-        m.put("judía", "mongeta");
-        m.put("guisante", "pèsol");
-        m.put("calabaza", "carabassa");
-        ArrayList <String> dic = new ArrayList<>(m.keySet());
-        Set<Integer> n = new HashSet<>();
-        while (n.size()<5) n.add((int) (Math.random() * (n.size() - 1 + 1) + 1));
-        for (int i = 0; i < 4; i++) {
-            System.out.println(m.get(n));
-            if (m.get(random).contains(sc.nextLine())) {
+        Map<String, String> dicMap = new HashMap<>();
+        dicMap.put("manzana", "poma");
+        dicMap.put("pera", "pera");
+        dicMap.put("naranja", "taronja");
+        dicMap.put("limón", "llimona");
+        dicMap.put("plátano", "plàtan");
+        dicMap.put("uva", "raïm");
+        dicMap.put("fresa", "maduixa");
+        dicMap.put("arándano", "blauet");
+        dicMap.put("frambuesa", "framboesa");
+        dicMap.put("piña", "pinya");
+        dicMap.put("melón", "meló");
+        dicMap.put("sandía", "síndria");
+        dicMap.put("zanahoria", "pastanaga");
+        dicMap.put("cebolla", "ceba");
+        dicMap.put("ajo", "all");
+        dicMap.put("pimiento", "pebrot");
+        dicMap.put("tomate", "tomàquet");
+        dicMap.put("patata", "patata");
+        dicMap.put("maíz", "blat de moro");
+        dicMap.put("judía", "mongeta");
+        dicMap.put("guisante", "pèsol");
+        dicMap.put("calabaza", "carabassa");
+        ArrayList <String> dic = new ArrayList<>(dicMap.keySet());
+        Set<Integer> Lnum = new HashSet<>();
+        while (Lnum.size()<5) Lnum.add((int) (Math.random() * (Lnum.size() + 1)));
+        for (int pos : Lnum) {
+            System.out.println(dic.get(pos));
+            System.out.print("traducció: ");
+            if (dicMap.get(dic.get(pos)).equals(sc.nextLine())) {
                 System.out.println("Correcte!");
-            }else System.out.println("Resposta incorrecta. La resposta correcta és "+m.entrySet().toString());   
+            }else System.out.println("Resposta incorrecta. La resposta correcta és "+dicMap.get(dic.get(pos)));   
+            
+            
+            
+            
         }
+        
     }
 }
