56539ffbfffb2f83b071d0b7ed312abbb4cfa1f0
sanand <andsansan5@alu.edu.gva.es>
2023-05-10 14:22:58 +0200

    d

--- 3raAVA/UF11/PartB/UF11_B07/src/uf11_b07/UF11_B07.java
+++ 3raAVA/UF11/PartB/UF11_B07/src/uf11_b07/UF11_B07.java
@@ -7,13 +7,14 @@ package uf11_b07;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
+
 
 /**
  *
@@ -21,21 +22,17 @@ import java.util.TreeMap;
  */
 public class UF11_B07 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void estadistiques(String path) {
+        final int FIRST = 10;
         Scanner sc = new Scanner(System.in);
-        File f = new File("DocumentsB/Llibres/lazarillo.txt");
-        //File f = new File("DocumentsB/Llibres/" + sc.nextLine());
+        File f = new File("DocumentsB/Llibres/"+path);
         Scanner rc = null;
         try {
             rc = new Scanner(f);
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
-        
+
         int nLines = 0;
         int nWords = 0;
         int nCharacters = 0;
@@ -45,30 +42,41 @@ public class UF11_B07 {
         while (rc.hasNextLine()) {
             nLines++;
             String[] words = rc.nextLine().split(" ");
-            
-            nWords +=words.length;
+
+            nWords += words.length;
             for (String wrd : words) {
                 doc.add(wrd);
                 noRpt.add(wrd);
                 String[] chr = wrd.split("");
                 nCharacters += chr.length;
             }
-            
-            
+
         }
         for (String str : noRpt) {
             pdm.put(str, Collections.frequency(doc, str));
         }
+        ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(pdm.entrySet());
+        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
         System.out.println("Llibre : " + f.getName());
         System.out.println("Línies totals : " + nLines);
         System.out.println("Nombre de paraules : " + nWords);
         System.out.println("Nombre de caracters : " + nCharacters);
-        System.out.println("Les 10 paraules mes comunes són : \n");
-        System.out.printf("%15s%20s%n","Paraules","Vegades");
-        for (Map.Entry m : pdm.entrySet()) {
-            System.out.printf(" %-30s%-20s%n", m.getKey(),m.getValue());
+        System.out.println("Les "+FIRST+" paraules mes comunes són : \n");
+        System.out.printf("%15s%20s%n", "Paraules", "Vegades");
+
+        for (int i = 0; i < FIRST; i++) {
+            System.out.printf(" %-30s%-20s%n", list.get(i).getKey(), list.get(i).getValue());
         }
-     
+        sc.close();
+        rc.close();
     }
-    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        File llibres = new File ("DocumentsB/Llibres");
+        String[] llibre = llibres.list();
+        for (String llibre1 : llibre) {
+            estadistiques(llibre1);
+        }
+    }
 }
