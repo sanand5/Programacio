8b9db937de1655a4626259eb42c117959ca4ac87
sanand <andsansan5@alu.edu.gva.es>
2023-05-26 21:18:10 +0200

    d

---
+++ 3raAVA/UF13/Prova01/src/prova01/Prova01.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prova01;
+
+
+import java.sql.*;
+        
+/**
+ *
+ * @author sanand
+ */
+public class Prova01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            String url= "jdbc:mysql://localhost:3306/prova";
+            Connection conn = DriverManager.getConnection(url, "root", "");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM venedors");
+    
+            
+            
+            while (rs.next()) {
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    System.out.print(rs.getString(i) + " - ");
+                }
+                System.out.println();
+            }
+            
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+}
