402f9bd449df35a58d921cdbb98f0acca08ab2d0
sanand <andsansan5@alu.edu.gva.es>
2023-05-29 17:52:55 +0200

    dç

--- 3raAVA/UF13/Prova02/src/prova02/Prova02.java
+++ 3raAVA/UF13/Prova02/src/prova02/Prova02.java
@@ -2,31 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package prova01;
-
-
+package prova02;
 import java.sql.*;
-        
 /**
  *
  * @author sanand
  */
-public class Prova01 {
+public class Prova02 {
 
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
         // TODO code application logic here
+        
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            String url= "jdbc:mysql://localhost:3306/prova";
+            String url = "jdbc:mysql://localhost:3306/prova";
             Connection conn = DriverManager.getConnection(url, "root", "");
-            Statement stmt = conn.createStatement();
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
             ResultSet rs = stmt.executeQuery("SELECT * FROM venedors");
-            
-            
+            int numcolumns = rs.getMetaData().getColumnCount();
+            rs.last();
+            rs.updateString("nom", "Paca JIJI");
+            rs.updateRow();
+            rs.beforeFirst();
             while (rs.next()) {
                 for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                     System.out.print(rs.getString(i) + " - ");
@@ -34,13 +34,12 @@ public class Prova01 {
                 System.out.println();
             }
             
+            rs.close();
             stmt.close();
             conn.close();
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
-        
-        
     }
     
 }
--- 3raAVA/UF13/Prova03/src/prova03/Prova03.java
+++ 3raAVA/UF13/Prova03/src/prova03/Prova03.java
@@ -2,45 +2,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package prova01;
-
-
+package prova03;
 import java.sql.*;
-        
 /**
  *
  * @author sanand
  */
-public class Prova01 {
+public class Prova03 {
 
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
         // TODO code application logic here
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            String url= "jdbc:mysql://localhost:3306/prova";
+            String url = "jdbc:mysql://localhost:3306/prova";
             Connection conn = DriverManager.getConnection(url, "root", "");
-            Statement stmt = conn.createStatement();
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
             ResultSet rs = stmt.executeQuery("SELECT * FROM venedors");
+            int numcolumns = rs.getMetaData().getColumnCount();
+            
+           
+            
+            rs.moveToInsertRow();
+            rs.updateString(2, "detgwerfsdf");
+            rs.updateDate(3, new Date(9999999999999L));
+            rs.insertRow();
+            rs.moveToCurrentRow();
             
             
-            while (rs.next()) {
-                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-                    System.out.print(rs.getString(i) + " - ");
-                }
-                System.out.println();
-            }
+            rs.absolute(4);
+            rs.deleteRow();
+//            for (int i = 0; i < 5; i++) {
+//                rs.last();
+//                
+//            }
             
+            rs.close();
             stmt.close();
             conn.close();
         } catch (Exception e) {
             System.out.println(e);
         }
-        
-        
     }
     
 }
